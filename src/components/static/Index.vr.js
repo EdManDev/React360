c6764908d114ac652d463a05e782d7970cd55bad
EdMan <mojave@EdMans-MBP.local>
2019-08-25 08:55:37 -0400

    first commit

---
+++ src/components/static/Index.vr.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class hello360 extends Component {
+	render() {
+		return (
+			<div>
+				<View>
+					<View>
+						<Text> Welcome to React 360</Text>
+					</View>
+				</View>
+			</div>
+		);
+	}
+}
+
+export default hello360;
